5ed48f3b827ba18c8ae42a159734e07828dd2f98
nathanial292 <nathanial55@gmail.com>
2020-02-18 19:16:21 +0000

    Basic caching, need to split headers

--- webproxy.py
+++ webproxy.py
@@ -11,15 +11,18 @@ class WebProxy:
         self.port = port
 
     def handleRequest(self, client, address):
-        #TODO: Caching
         data = client.recv(1024).decode()
 
         # Get hostname from request
         requestName = data.split(' ')[1].split("http://")[1]
 
+        # Get the file requested from host
+        fileToServe = requestName.split("/")[1]
+
         # Remove trailing / on hostname
         if (requestName[len(requestName)-1] == "/"):
             requestName = requestName[:-1]
+            fileToServe = "index" # If base requested, set to empty
         
         # Convert to IP
         requestAddress = socket.gethostbyname(requestName)
@@ -28,17 +31,32 @@ class WebProxy:
         print("Method: {requestMethod}".format(requestMethod=requestMethod))
         print("Host: {requestName} - ({requestAddress})".format(requestName=requestName, requestAddress=requestAddress))
         
-        # Use temporary socket to get data from host
-        tempSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tempSocket.connect((requestAddress, 80)) # Connect to requested host
-        tempSocket.sendall(data.encode()) # Forward data from client to host
+        cache = requestName+"/"+fileToServe+".html"
+        # Try to open cache, if exists use that, otherwise fetch new data
+        try:
+            print("Attepting to open from cache: '"+cache+"'")
+            file = open("captive.apple.com/index.html","r")
+            response = file.read().encode()
+            file.close()             
+        except:
+            print("No cache found for: '"+cache+"'. Creating...")
+            try:
+                os.mkdir(requestName) # Try to create folder for site
+            except:
+                print("Folder already exists: "+requestName+". Creating cache file for "+fileToServe+".html")
+            cacheFile = open(cache, "w") # Create the file
 
-        response = ""
-        if requestMethod == "GET":
-            while True:
-                response = tempSocket.recv(1024) # Get data from host
-                if not response: break
-                client.send(response) # Send data back to client
+            # Use temporary socket to get data from host
+            tempSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tempSocket.connect((requestAddress, 80)) # Connect to requested host
+            tempSocket.sendall(data.encode()) # Forward data from client to host
+
+            #if requestMethod == "GET":
+
+            response = tempSocket.recv(1024) # Get data from host
+            cacheFile.write(response.decode())
+
+        client.send(response) # Send data back to client
         client.close() # Close socket connection
 
     def startProxy(self):
